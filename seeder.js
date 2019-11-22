dd132b5136a3fb6c62c4f45019766aed48eada7f
Gavin Quirk <gavinquirk@gmail.com>
2019-11-22 12:47:43 -0800

    db seeder

---
+++ seeder.js
@@ -0,0 +1,56 @@
+// *** Mongoose DB Seeder ***
+// from root folder, run:
+// "node seeder -import" to import bootcamps to db
+// "node seeder -delete" to delete bootcamps from db
+
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Load models
+const User = require('./models/User');
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+// Read JSON files
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
+// Import into DB
+const importData = async () => {
+  try {
+    await User.create(users);
+    console.log('Data Imported...'.green.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await User.deleteMany();
+    console.log('Data Deleted...'.red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+if (process.argv[2] === '-import') {
+  importData();
+} else if (process.argv[2] === '-delete') {
+  deleteData();
+}
