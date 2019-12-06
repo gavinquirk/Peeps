57f6d9a3ee090ca63a2f794352d9ae90e5899f0b
Gavin Quirk <gavinquirk@gmail.com>
2019-12-05 18:16:19 -0800

    centered login and register forms

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import './Login.css';
+
 export default class Login extends Component {
   state = {
     email: '',
@@ -24,37 +26,39 @@ export default class Login extends Component {
 
     return (
       <div className='Login'>
-        <h1>Login</h1>
-        <form className='form' onSubmit={e => this.onSubmit(e)}>
-          <div className='form-group'>
-            <label htmlFor='email'>Email Address</label>
-            <input
-              name='email'
-              type='email'
-              className='form-control'
-              id='email'
-              placeholder='Enter your email'
-              value={email}
-              onChange={e => this.onChange(e)}
-              required
-            />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='password'>Password</label>
-            <input
-              name='password'
-              type='password'
-              className='form-control'
-              id='password'
-              placeholder='Enter your password'
-              minLength='5'
-              value={password}
-              onChange={e => this.onChange(e)}
-              required
-            />
-          </div>
-          <input type='submit' className='btn btn-primary' value='Login' />
-        </form>
+        <div className='login-container'>
+          <h1>Login</h1>
+          <form className='form' onSubmit={e => this.onSubmit(e)}>
+            <div className='form-group'>
+              <label htmlFor='email'>Email Address</label>
+              <input
+                name='email'
+                type='email'
+                className='form-control'
+                id='email'
+                placeholder='Enter your email'
+                value={email}
+                onChange={e => this.onChange(e)}
+                required
+              />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='password'>Password</label>
+              <input
+                name='password'
+                type='password'
+                className='form-control'
+                id='password'
+                placeholder='Enter your password'
+                minLength='5'
+                value={password}
+                onChange={e => this.onChange(e)}
+                required
+              />
+            </div>
+            <input type='submit' className='btn btn-primary' value='Login' />
+          </form>
+        </div>
       </div>
     );
   }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -4,6 +4,8 @@ import { setAlert } from '../../actions/alert';
 import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
+import './Register.css';
+
 class Register extends Component {
   state = {
     name: '',
@@ -38,64 +40,66 @@ class Register extends Component {
 
     return (
       <div className='Register'>
-        <h1>Register</h1>
-        <form className='form' onSubmit={e => this.onSubmit(e)}>
-          <div className='form-group'>
-            <label htmlFor='name'>Name</label>
-            <input
-              name='name'
-              type='text'
-              className='form-control'
-              id='name'
-              placeholder='Enter your name'
-              value={name}
-              onChange={e => this.onChange(e)}
-              required
-            />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='email'>Email Address</label>
-            <input
-              name='email'
-              type='email'
-              className='form-control'
-              id='email'
-              placeholder='Enter your email'
-              value={email}
-              onChange={e => this.onChange(e)}
-              required
-            />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='password'>Password</label>
-            <input
-              name='password'
-              type='password'
-              className='form-control'
-              id='password'
-              placeholder='Enter your password'
-              minLength='5'
-              value={password}
-              onChange={e => this.onChange(e)}
-              required
-            />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='password-2'>Confirm Password</label>
-            <input
-              name='password2'
-              type='password'
-              className='form-control'
-              id='password2'
-              placeholder='Confirm your password'
-              minLength='5'
-              value={password2}
-              onChange={e => this.onChange(e)}
-              required
-            />
-          </div>
-          <input type='submit' className='btn btn-primary' value='Register' />
-        </form>
+        <div className='register-container'>
+          <h1>Register</h1>
+          <form className='form' onSubmit={e => this.onSubmit(e)}>
+            <div className='form-group'>
+              <label htmlFor='name'>Name</label>
+              <input
+                name='name'
+                type='text'
+                className='form-control'
+                id='name'
+                placeholder='Enter your name'
+                value={name}
+                onChange={e => this.onChange(e)}
+                required
+              />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='email'>Email Address</label>
+              <input
+                name='email'
+                type='email'
+                className='form-control'
+                id='email'
+                placeholder='Enter your email'
+                value={email}
+                onChange={e => this.onChange(e)}
+                required
+              />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='password'>Password</label>
+              <input
+                name='password'
+                type='password'
+                className='form-control'
+                id='password'
+                placeholder='Enter your password'
+                minLength='5'
+                value={password}
+                onChange={e => this.onChange(e)}
+                required
+              />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='password-2'>Confirm Password</label>
+              <input
+                name='password2'
+                type='password'
+                className='form-control'
+                id='password2'
+                placeholder='Confirm your password'
+                minLength='5'
+                value={password2}
+                onChange={e => this.onChange(e)}
+                required
+              />
+            </div>
+            <input type='submit' className='btn btn-primary' value='Register' />
+          </form>
+        </div>
       </div>
     );
   }
