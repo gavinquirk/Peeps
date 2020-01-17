4f14a4f3ceb445fa0d3c98c42cd9162f2274b990
Gavin Quirk <gavinquirk@gmail.com>
2020-01-16 16:01:08 -0800

    navbar switched to reactstrap

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,67 +1,46 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
 
-export default class NavBar extends Component {
-  render() {
-    return (
-      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-        <a className='navbar-brand' href='http://localhost:3000'>
-          Peeps
-        </a>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-toggle='collapse'
-          data-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
+const NavBar = props => {
+  const [isOpen, setIsOpen] = useState(false);
 
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul className='navbar-nav mr-auto'>
-            <li className='nav-item '>
-              <Link className='px-3 py-1 text-decoration-none text-dark' to='/'>
-                Home
-              </Link>
-            </li>
-            <li className='nav-item '>
-              <Link
-                className='px-3 py-1 text-decoration-none text-dark'
-                to='/dashboard'
-              >
-                Dashboard
-              </Link>
-            </li>
-            <li className='nav-item '>
-              <Link
-                className='px-3 py-1 text-decoration-none text-dark'
-                to='/find'
-              >
-                Find
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                className='px-3 py-1 text-decoration-none text-dark'
-                to='/login'
-              >
-                Login
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                className='px-3 py-1 text-decoration-none text-dark'
-                to='/register'
-              >
-                Register
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
-}
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <>
+      <Navbar color='light' light expand='md'>
+        <NavbarBrand href='/'>Peeps</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className='mr-auto' navbar>
+            <NavItem>
+              <NavLink href='/'>Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='/find'>Find</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='/dashboard'>Dashboard</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='/login'>Login</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href='/register'>Register</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavBar;
